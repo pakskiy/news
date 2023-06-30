200e7282ac2a8cee94bc359f7b605df27cf45a17
Oleg <oleg@shazzle.com>
2023-06-30 05:27:29 +0500

    Added functional for Login module: controller, dto, service, entity and repository

---
+++ src/main/java/com/pakskiy/news/dto/LoginRequestDto.java
@@ -0,0 +1,14 @@
+package com.pakskiy.news.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String sessionId;
+}
